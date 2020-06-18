ff9ef087d71df89a7e4357a69d730d009681e735
martinquijano5 <tutequijano@gmail.com>
2020-06-18 18:25:16 -0300

    intento de menu

---
+++ Proyecto/Assets/Cambio_scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagemen;
+
+public class Cambio_scene : MonoBehaviour
+{
+
+public void PlayGame(string NombreEscena)
+    {
+        
+    }
+
+}
